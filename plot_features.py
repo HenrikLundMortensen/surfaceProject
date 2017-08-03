9c8df1bec5c08492f16402803022c9013dc227bb
MaltheBisbo <30654168+MaltheBisbo@users.noreply.github.com>
2017-08-03 12:43:38 +0200

    update

--- plot_features.py
+++ plot_features.py
@@ -1,1 +1,23 @@
-ss
+import numpy as np
+import matplotlib as mpl
+mpl.use("TkAgg")
+import matplotlib.pyplot as plt
+import calcenergy as ce
+N=5
+image = ce.randSurface(N)
+print(image)
+"""
+f(I,a,f)
+"""
+x=np.arange(1,101)
+y1= 20 + 3 * x + np.random.normal(0, 60, 100)
+y2= 20 + 3 * x + np.random.normal(0, 60, 100)
+
+plt.figure(1)
+plt.plot(x,y1,"o")
+plt.figure(2)
+plt.plot(x,y2,"x")
+plt.show()
+
+def plotFeatures(f):
+    d=1
