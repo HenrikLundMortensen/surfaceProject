5a5a36c2c87e185305084a94c5c63d77b7d00be5
Henrik Lund Mortensen <HenrikLundMortensen@gmail.com>
2017-08-03 15:29:52 +0200

    More comments in plotGrid.py

--- plotGrid.py
+++ plotGrid.py
@@ -1,10 +1,27 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 
+
 def plotGrid(g,fig):
-    # Rotate grid, such that the view is correct
+    """
+    Function that plots the grid. 
+
+    First, use initializePlotGridFigure to create a figure instance. The plotGrid function takes this figure as an 
+    argument and updates the plots. 
+    
+
+    Input:
+    g: Grid
+    fig: Figure (created with initializePlotGridFigure)
+
+    Output:
+    None
+    """
+    # Rotate grid, such that the view is correct (Maybe not necessary)
     rotg = np.rot90(g,3)
 
+    # Extend the matrix, so we plot 9 unit cells
     tmp = np.concatenate((rotg,rotg),axis=0)
     tmp = np.concatenate((tmp,rotg),axis=0)
     tmp2 = np.concatenate((tmp,tmp),axis=1)
@@ -26,14 +43,17 @@ def plotGrid(g,fig):
     CoordsAg = np.array(list( np.dot(shearMat,i) for i in PairsAg)).T
     CoordsO = np.array(list( np.dot(shearMat,i) for i in PairsO)).T
 
+    CoordsAg = np.subtract(CoordsAg,5)
+    CoordsO = np.subtract(CoordsO,5)
+    
     # Update the x and y values of the plots
     ax = fig.gca()
 
-    # ax.get_children()[0] are for the Ag atoms
+    # ax.get_children()[1] are for the Ag atoms
     ax.get_children()[0].set_xdata(CoordsAg[0])
     ax.get_children()[0].set_ydata(CoordsAg[1])
 
-    # ax.get_children()[1] are for the O atoms
+    # ax.get_children()[2] are for the O atoms
     ax.get_children()[1].set_xdata(CoordsO[0])
     ax.get_children()[1].set_ydata(CoordsO[1])
 
@@ -42,25 +62,27 @@ def plotGrid(g,fig):
 
 
 def initializePlotGridFigure(N):
+    """
+    Input:
+    N: System size
+
+    Output:
+    plt.figure instance
+    """
     fig = plt.figure()
 
     ax = fig.gca()
 
+    # Set background color and x and y limits
     ax.set_facecolor((0.9,0.8,1))
-    ax.set_xlim([N,3*N])
-    ax.set_ylim([0,2*N])
+    ax.set_xlim([0,1.5*N+2])
+    ax.set_ylim([-2,N])
 
     
     # Create two plot instances with no data
     ax.plot([],[],'bo',markersize =15)
     ax.plot([],[],'ro',markersize =7)
 
-    # Define the shearing matrix
-    # shearMat = np.array([[1, 0.5],[0,1]])
-
-    # cornerCoords = [[N, 2*N,N,2*N],[N,N,2*N,2*N]]
-    # ax.plot(cornerCoords[0],cornerCoords[1],'b',linewidth=2)
-            
     return fig
 
 
