5a0a9cbb3bac31e3b535f9e4382fe8d7e020fe49
MaltheBisbo <30654168+MaltheBisbo@users.noreply.github.com>
2017-08-03 13:02:00 +0200

    update

--- plot_features.py
+++ plot_features.py
@@ -6,18 +6,19 @@ import calcenergy as ce
 N=5
 image = ce.randSurface(N)
 print(image)
+"""                                                                                                                                          
+Inputs 3D array f(image,atom,feature_vector)                                                                                                 
+                                                                                                                                             
+In seperate figures the Ag and O atoms are plotted in the feature space (2D apace of surrounding atomic densies).                            
 """
-f(I,a,f)
-"""
-x=np.arange(1,101)
-y1= 20 + 3 * x + np.random.normal(0, 60, 100)
-y2= 20 + 3 * x + np.random.normal(0, 60, 100)
-
-plt.figure(1)
-plt.plot(x,y1,"o")
-plt.figure(2)
-plt.plot(x,y2,"x")
-plt.show()
 
 def plotFeatures(f):
-    d=1
+    N_images, N_atoms = size(f,0), size(f,1)
+    fori in range(N_images): # Iterate through images                                                                                       
+    for j in range(N_atoms): # Iterate througt atoms in a specific image                                                                 
+            if f[i,j,2] == 47: # plot Ag                                                                                                     
+                plt.figure(1)
+plt.plot(f[i,j,0],f[i,j,1])
+else f[i,j,2] == 8: # plot O                                                                                                     
+    plt.figure(2)
+plt.plot(f[i,j,0],f[i,j,1])
