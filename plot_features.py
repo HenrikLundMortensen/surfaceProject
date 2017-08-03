319eff7502cf414625f137b53f47ddb37ee779f9
MaltheBisbo <30654168+MaltheBisbo@users.noreply.github.com>
2017-08-03 13:41:38 +0200

    update

--- plot_features.py
+++ plot_features.py
@@ -1,24 +1,31 @@
 import numpy as np
-import matplotlib as mpl
-mpl.use("TkAgg")
 import matplotlib.pyplot as plt
 import calcenergy as ce
-N=5
-image = ce.randSurface(N)
-print(image)
+
 """                                                                                                                                          
 Inputs 3D array f(image,atom,feature_vector)                                                                                                 
                                                                                                                                              
 In seperate figures the Ag and O atoms are plotted in the feature space (2D apace of surrounding atomic densies).                            
 """
-
+        
 def plotFeatures(f):
-    N_images, N_atoms = size(f,0), size(f,1)
-    fori in range(N_images): # Iterate through images                                                                                       
-    for j in range(N_atoms): # Iterate througt atoms in a specific image                                                                 
-            if f[i,j,2] == 47: # plot Ag                                                                                                     
+    N_images, N_atoms = np.size(f,0), np.size(f,1)
+    for i in range(N_images): # Iterate through images 
+        for j in range(N_atoms): # Iterate througt atoms in a specific image
+            if f[i,j,2] == 0: # plot Ag                                                                                                    
                 plt.figure(1)
-plt.plot(f[i,j,0],f[i,j,1])
-else f[i,j,2] == 8: # plot O                                                                                                     
+                plt.plot(f[i,j,0],f[i,j,1],"o",color="blue")
+            elif f[i,j,2] == 1: # plot O
+                plt.figure(2)
+                plt.plot(f[i,j,0],f[i,j,1],"o",color="red")
+    plt.figure(1)
+    plt.title("Feature space of Ag atoms")
+    plt.xlabel("density of O atoms")
+    plt.ylabel("density of Ag atoms")
     plt.figure(2)
-plt.plot(f[i,j,0],f[i,j,1])
+    plt.title("Feature space of Ag atoms")
+    plt.xlabel("density of O atoms")
+    plt.ylabel("density of Ag atoms")
+    plt.show()
+
+plotFeatures(f)
