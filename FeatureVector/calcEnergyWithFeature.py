4ebb4c8607fb74acc4556661c3e3be07f244c42e
SoerenMeldgaard <sm@phys.au.dk>
2017-08-04 11:24:42 +0200

    MonteCarlo with feature

--- FeatureVector/calcEnergyWithFeature.py
+++ FeatureVector/calcEnergyWithFeature.py
@@ -24,8 +24,6 @@ def EFeature(f):
 
     return E
 
-    
-
 def EFeatureGrid(g):
     """
     Calculate the energy given a grid. The energy is calculated with our own rules based
@@ -45,15 +43,10 @@ def EFeatureGrid(g):
     
 def EBondFeature(f):
     """
-    Calculates the energy contribution from a single feature vector. The rules are completely                                                     
-    made up.                                                                                                                                      
-    Input:                                                                                                                                        
-    f: Featurevector ( [O density, Ag density, Atomic number,O-O Lenght,Ag-Ag Length,Ag-O Length] )                                              
-
-    Output:                                                                                                                                       
-    E: Energy contribution of a single feature vector
+    Calculates the energy contribution from a single feature vector. The rules are completely                                        made up.                                                                                                                                      
+    Input:                                                                                                                           f: Featurevector ( [O density, Ag density, Atomic number,O-O Lenght,Ag-Ag Length,Ag-O Length] )                                              
+    Output:                                                                                                                          E: Energy contribution of a single feature vector
     """
-    
     E = 0
 
     if f[2] == 8:
@@ -67,4 +60,18 @@ def EBondFeature(f):
         E -= np.exp(-(f[3] - 1)**2/(0.8**2)) + np.exp(-(f[3] - 2)**2/(0.8**2))
 	E -= np.exp(-(f[4]-(3+2*np.sqrt(3)+2))**2/(0.8**2)) + np.exp(-(f[4]-1)**2/(0.8**2))
 	E -= np.exp(-(f[5] - 2*(1+np.sqrt(3)))**2/(0.8**2)) + np.exp(-(f[5] - 2*(1+2*np.sqrt(3)+2))**2/(0.8**2))
-    return E~
+    return E
+
+def EBondFeatureGrid(g):
+    """                                                                                                                           
+    Calculate the energy given a grid. The energy is calculated with our own rules based                                             on the feature vectors.
+    Input:
+    g: numpy array grid
+
+    Output:
+    E: sum of energy contributions from all feature vectors
+    """
+    E = 0
+    for f in getBondFeatureVectors([g])[0]:
+        E += EBondFeature(f)
+    return E
