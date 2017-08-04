e1496569cde6c957a6f777870e9b666ed8bad177
Henrik Lund Mortensen <HenrikLundMortensen@gmail.com>
2017-08-04 14:24:20 +0200

    Added plotGrid as a package.

---
+++ plotGrid/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='plotGrid',
+      version='0.1',
+      description='Tools for plotting grid',
+      author='Henrik Lund Mortensen',
+      author_email='hlm@phys.au.dk',
+      packages=['plotGrid'],
+      zip_safe=False)
