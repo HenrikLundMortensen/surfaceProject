fb1c23c1015603deea1027692180f421cbc25d19
Henrik Lund Mortensen <HenrikLundMortensen@gmail.com>
2017-08-07 09:13:56 +0200

    Added comments and removed test- and plotting part

--- surfaceProject/henriksKmeans/kmeans.py
+++ surfaceProject/henriksKmeans/kmeans.py
@@ -2,8 +2,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.cluster.vq import kmeans2
 import time
-
-
     
 
 def featureVecDist(f1,f2):
@@ -63,112 +61,34 @@ def randInitFlist(flist,K):
     return initFlist
 
 
-def getNearestCenter(f,fcs):
-    """
-    For feature f calculate the nearest center in fcs list
+
+
+def henrikskmeans(flist,K):
     """
 
+    Finds K clusters in list of feature vectors.
+
+    Input:
+    flist: numpy array with featurevectors, such that flist = np.array([f1,f2,f3,...])
+    K: Number of clusters
+
+    Output: [clist,fcs]
     
-    dist = np.array([])
-    for fc in fcs:
-        dist = np.append(dist,featureVecDist(f,fc))
+    clist: Contains an number representing which cluster the n'th feature vector belongs to
+    If feature vector 1 and 3 belongs to first cluster and feature vector 2 belongs to the second, clist is [0,1,0]
 
-        
-    return np.argmin(dist)
-    
 
-def getIntermediateClusters(flist,fcs):
+    fcs: Cluster centers, [clustercenter_1,clustercenter_2,...]
+
     """
-    """
-
-    clist = []
-    for f in flist:
-        clist.append(getNearestCenter(f,fcs))
-
-    return np.array(clist)
-
-
-def getNewCenter(cflist):
-    """
-    """
-
-    return 1/len(cflist) * sum(cflist)
-
-
-    
-def Mykmeans(flist,K):
-    """
-    """
-
     # Get some starting point for feature centers
     fcs = randInitFlist(flist,K)
-
-    clist = [np.array(list(np.argmin(list(featureVecDist(f,fc) for fc in fcs)) for f in flist)),
-             'Some placeholder value']
-    
-    i = 0
-    # clistref = np.zeros(len(flist))
-    # clist = np.random.rand(len(flist))
-    while not np.array_equal(clist[0],clist[1]):
-
-        # Calculate current clusters
-        
-
-        # Stop if it has not changed since last iteration
-        # if :
-        #     break
-
-        # If not, save a copy of the current cluster list
-        # clistref = clist
-
-        # Calculate new feature centers
-        for k in range(K):
-            fcs[k] = getNewCenter(flist[np.where(clist[0] == k)])
-
-        clist = [getIntermediateClusters(flist,fcs),clist[0]]
-        
-        i += 1
-        
-    return [clist,fcs]
-
-
-
-
-
-
-def kmeanscompact(flist,K):
-    """
-    """
-
-    fig = plt.figure()
-    ax = fig.gca()
-
-    
-    # Get some starting point for feature centers
-    fcs = randInitFlist(flist,K)
-
-    Nf = len(flist[0])
-
     
     clist = [np.array(list(np.argmin(list(featureVecDist(f,fc) for fc in fcs)) for f in flist)),
              'Some placeholder value']
-
-
-    colorlist = ['bo','ro','yo']
-    pltlist = [0]*K
-    for k in range(K):
-        pltlist[k], =ax.plot(np.transpose(flist[np.where(clist[0] == k)])[0],np.transpose(flist[np.where(clist[0] == k)])[1],colorlist[k],markersize=5)
-
-    fcsplt, = ax.plot(np.transpose(fcs)[0],np.transpose(fcs)[1],'k*',markersize=20)
-    
-    plt.pause(1)
-    
-    # clist = [np.array(list(np.argmin(list(featureVecDist(f,fc) for fc in fcs)) for f in flist)),
-    #          'Some placeholder value']
-
 
     # While the clusters change between iterations
-    i = 0
+
     while not np.array_equal(clist[0],clist[1]):
         
         # Find new centers
@@ -176,112 +96,9 @@ def kmeanscompact(flist,K):
             cflist = flist[np.where(clist[0]==k)]
             fcs[k] = 1/len(cflist) * sum(cflist)
 
-        
         # Calculate new clusters
-        t1 = time.time()
         clist = [np.array(list(np.argmin(list(featureVecDist(f,fc) for fc in fcs)) for f in flist)),
                  clist[0]]
-        t2 = time.time()
-        print('Calculate new clusters:%g ' %(t2-t1))
 
-
-        # plot
-
-        for k in range(K):
-            pltlist[k].set_xdata(np.transpose(flist[np.where(clist[0] == k)])[0])
-            pltlist[k].set_ydata(np.transpose(flist[np.where(clist[0] == k)])[1])
-
-        fcsplt.set_xdata(np.transpose(fcs)[0])
-        fcsplt.set_ydata(np.transpose(fcs)[1])
-
-        plt.pause(1)
-            
-        
-
-
-
-        
-        i += 1
         
     return [clist[0],fcs]
-
-
-# TESTING
-
-
-        
-
-
-
-# Generate two groups of feature vectors with two features for testing
-flist = []
-
-# First group 
-
-for i in range(500):
-    flist.append([np.random.rand()*(17-5)  + 5  ,np.random.rand()*(17-5)  + 5 ])
-        
-
-# Second group
-for i in range(500):
-    flist.append([np.random.rand()*(20-15)  + 15  ,np.random.rand()*(20-12)  + 12 ])
-
-
-flist = np.array(flist)
-
-K = 2
-
-
-# fig = plt.figure()
-# ax = fig.gca()
-# ax.plot(np.transpose(flist)[0],np.transpose(flist)[1],'bo',markersize=5)
-
-
-a = time.time()
-for i in range(1):
-    kmeans2(flist,3)
-t1 = time.time()-a
-
-
-
-
-a = time.time()
-for i in range(1):
-    kmeanscompact(flist,3)
-t2 = time.time()-a
-
-
-# [clist,fcs] = kmeanscompact(flist,K)
-# [fcs2,clist2] = kmeans2(flist,K)
-[fcs,clist] = kmeans2(flist,K)
-
-
-# print(t1*10)
-# print(t2*10)
-# print(it1)
-# print(it2)
-
-
-
-
-
-# for i in range(10):
-    
-#     [clist,fcs] = kmeans(flist,K)
-
-
-#     plt.cla()
-# colorlist = ['bo','ro','yo']
-# for k in range(K):
-#     ax.plot(np.transpose(flist[np.where(clist == k)])[0],np.transpose(flist[np.where(clist == k)])[1],colorlist[k],markersize=5)
-#     fcsplt, = ax.plot(np.transpose(fcs)[0],np.transpose(fcs)[1],'k*',markersize=20)
-
-        
-# #     plt.pause(0.4)
-
-# print('Done!')
-# plt.show()
-
-
-
-
