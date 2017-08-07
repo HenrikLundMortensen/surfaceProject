58559f87bf9994c4e155b44ac9eaf184cf757fb3
MaltheBisbo <30654168+MaltheBisbo@users.noreply.github.com>
2017-08-07 09:10:34 +0200

    update

--- test_folder/test.py
+++ test_folder/test.py
@@ -2,12 +2,8 @@ import surfaceProject.energycalculations.calcenergy as ce
 import surfaceProject.featureVector.featureVectors as fv
 surf = ce.randSurface(5)
 print(surf)
-<<<<<<< HEAD
 #f = fv.getBondFeatureVectorsSingleGrid(surf)
 #print(f)
-=======
+
 f = fv.getBondFeatureVectorsSingleGrid(surf)
 print(f)
-
-
->>>>>>> 3ee72a9aa01a6f3f88f713fea1642228d80de151
