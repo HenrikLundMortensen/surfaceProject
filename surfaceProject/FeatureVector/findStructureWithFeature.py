76457ee29c0638cbf77edf08c6902eeab5e9c620
SoerenMeldgaard <sm@phys.au.dk>
2017-08-08 16:25:08 +0200

    Minor documentation

--- surfaceProject/FeatureVector/findStructureWithFeature.py
+++ surfaceProject/FeatureVector/findStructureWithFeature.py
@@ -2,29 +2,34 @@ from surfaceProject.FeatureVector.calcEnergyWithFeature import *
 import random
 import numpy as np
 
-def randSurface(N): # Creates random surface matrix
+
+def randSurface(N):
+    '''' Creates a random surface of size N. The standard rules for number of
+    oxygen, silver and empty sites are obeyed'''
     o = 3*N - 9
     ag = N*N - 3*N + 2
     surface = np.zeros(N*N)
-    for i in range(0,ag):
+    for i in range(0, ag):
         surface[i] = 1
-    for i in range(ag,ag+o):
+    for i in range(ag, ag+o):
         surface[i] = 2
     surface = np.random.permutation(surface)
-    surface = np.reshape(surface,(N,N))
+    surface = np.reshape(surface, (N, N))
     return surface
 
-def shuffle(surface,M): # Shuffle M atoms on the surface                                                                          
+
+def shuffle(surface, M):
+    '''' Shuffles M atoms in place on the surface'''
     N = surface.shape[1]
     L = []
     x = 0
     while x < M:
-        i = random.randint(0,N-1)
-        j = random.randint(0,N-1)
-        if (i,j) not in L:
-            L.append((i,j))
+        i = random.randint(0, N-1)
+        j = random.randint(0, N-1)
+        if (i, j) not in L:
+            L.append((i, j))
             x += 1
-    shuffleArray = np.empty([M,3])
+    shuffleArray = np.empty([M, 3])
     for i in range(M):
             shuffleArray[i,0] = surface[L[i][0],L[i][1]]
             shuffleArray[i,1] = L[i][0]
@@ -33,7 +38,12 @@ def shuffle(surface,M): # Shuffle M atoms on the surface
     for i in range(M):
         surface[L[i][0],L[i][1]] = shuffleArray[i,0]
 
-def findOptimum(size):  # Find optimal structure                                                                                  
+        
+def findOptimum(size):
+    '''' Returns the optimal structure given a size.
+    This is configured using a specific energy expression (EBondFeatureGrid)
+    and thus if this expression is modified the algorithm will fail. It is
+    also optimzized for 5 x 5, and will probably not work for other sizes'''
     surface = randSurface(size)
     iter = 0
     E2 = 0
@@ -53,7 +63,12 @@ def findOptimum(size):  # Find optimal structure
             break
     return surface
 
-def generateTraining(surfSize,setSize):
+
+def generateTraining(surfSize, setSize):
+    ''''Produces setSize number of surface grids. The grids are found
+    based on a metropolis monte carlo method searching for the
+    optimal structure. The grids together with the energy for each
+    grid (based on feature vectors) are returned in random order'''
     surface = randSurface(surfSize)
     set = [0]*setSize
     energy = np.zeros(setSize)
