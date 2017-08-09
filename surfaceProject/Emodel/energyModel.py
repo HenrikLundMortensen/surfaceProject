efb9d18ae11c9fd9eaa55bbb79090ab2ef4e88e1
Henrik Lund Mortensen <HenrikLundMortensen@gmail.com>
2017-08-09 09:00:32 +0200

    Rewrote, but still does not work properly

--- surfaceProject/Emodel/energyModel.py
+++ surfaceProject/Emodel/energyModel.py
@@ -3,6 +3,9 @@ import warnings
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 import scipy.linalg as linalg
 from matplotlib import pyplot as plt
+import surfaceProject.FeatureVector.findStructureWithFeature as fs
+import surfaceProject.FeatureVector.featureVector as fv
+from surfaceProject.FeatureVector.getClusterNumberMatrix import *
 
 
 def createEnergyModel(CNmatrix,Elist):
@@ -15,7 +18,7 @@ def createEnergyModel(CNmatrix,Elist):
     EC: Energy associated with each cluster
     """
 
-    EC = np.dot(linalg.pinv(cM),EV)
+    EC = np.dot(linalg.pinv(CNmatrix),Elist)
     return EC
 
 
@@ -29,7 +32,7 @@ def getEnergyFromModel(CN,EC):
     Emodel: Energy predicted by the model
     """
 
-    Emodel = np.dot(Nclist,EW)
+    Emodel = np.dot(CN,EC)
     return Emodel
     
 
@@ -40,21 +43,46 @@ def getEnergyFromModel(CN,EC):
 if __name__ == '__main__':
 
 
+    # Grid size
+    N = 5
+    
     # Number of clusters
     K = 10
-    errorlist = []
-    for NT in list(range(K,1000)):
-        error = []
-        # Energy vector
-        EV = np.random.rand(NT)
-        cM = np.random.rand(NT,K)
-        EW = createEnergyModel(cM,EV)
-        Emodel = getEnergyFromModel(cM[0],EW)
-        for i in range(NT):
-            error.append((EV[i]-Emodel)**2)
 
-        errorlist.append(sum(error)/NT)
+    errorlist = []
+    i = 0;
+    
+    for NTrain in [100,200,300,400,500,600,700,800]:
+        print(i)
+        error = []
         
-    plt.plot(list(range(K,1000)),errorlist)
-    plt.show()
+        # Size of set
+        NSet = 1000
+
+        # Size of training set
+        # NTrain = 80
+        NTest = 200
+
+        # Generate traning set
+        I, E = fs.generateTraining(N,NSet)
+
+        ITrain = I[0:NTrain]
+        ETrain = E[0:NTrain]
+        ITest = I[NSet-NTest:NSet]
+        ETest = E[NSet-NTest:NSet]
+
+        FTrain = fv.getBondFeatureVectors(ITrain)
+        FTest = fv.getBondFeatureVectors(ITest)
+        
+
+        clusterNumMatTrain,kmeans_result = getClusterNumberMatrixTraining(FTrain,K)
+        clusterNumMatTest = getClusterNumberMatrix(FTest,kmeans_result,K)    
+    
+        EC = createEnergyModel(clusterNumMatTrain,ETrain)
+
+        EModel = np.dot(clusterNumMatTest,EC)
+        error = np.dot(ETest-EModel,ETest-EModel)/NTrain
+        errorlist.append(error)
+        i += 1
+        
     
